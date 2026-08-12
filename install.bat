86e2c8e53c13e05bb0427231cca7520a30bc0981
GaleGit <59199738+GaleGit@users.noreply.github.com>
2026-08-12 16:18:25 +0300

    Modify install.bat for task name and shortcut creation
    Updated task scheduler task name to 'dynamic-refresh' and added a command to create a desktop shortcut for toggling refresh rate.

--- install.bat
+++ install.bat
@@ -12,7 +12,10 @@ mkdir "C:\Program Files\QRes" 2>nul
 xcopy "%~dp0*" "C:\Program Files\QRes\" /Y /E /I
 
 :: Import Task Scheduler XML directly into Windows
-schtasks /create /tn "DynamicRefresh" /xml "C:\Program Files\QRes\taskschd.xml" /F
+schtasks /create /tn "dynamic-refresh" /xml "C:\Program Files\QRes\taskschd.xml" /F
+
+:: Create Desktop Shortcut
+powershell -Command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('\%USERPROFILE\%\Desktop\Toggle Refresh Rate.lnk');$s.TargetPath='wscript.exe'; $s.Arguments='\"C:\Program Files\QRes\qres.vbs\"'; $s.WorkingDirectory='C:\Program Files\QRes'; $s.IconLocation='C:\Program Files\QRes\QRes.exe,0';$s.Save()"
 
 echo.
 echo =========================================================
